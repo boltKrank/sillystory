e9e72547dba67fd83a2277b1541b34793205c26c
Stephen P. Potter <spp@unixsa.net>
2020-12-30 19:56:02 +1100

    minor update

--- sillstory.java
+++ sillstory.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import javax.sql.rowset.serial.SerialArray;
+
 class Sillystory {
 
 
@@ -73,12 +75,14 @@ class Sillystory {
 
          System.out.println("Enter 3 adjectives: ");
          String adj1 = myScanner.nextLine();
-         String adj1 = myScanner.nextLine();
-         String adj1 = myScanner.nextLine();
-         String adj1 = myScanner.nextLine();
-         String adj1 = myScanner.nextLine();
-         String adj1 = myScanner.nextLine();
-         String adj1 = myScanner.nextLine();
+         String adj2 = myScanner.nextLine();
+         String adj3 = myScanner.nextLine();
+         String adj4 = myScanner.nextLine();
+         String adj5 = myScanner.nextLine();
+         String adj6 = myScanner.nextLine();
+         String adj7 = myScanner.nextLine();
+         String adj8 = myScanner.nextLine();
+         String adj9 = myScanner.nextLine();
 
          System.out.println("Enter 3 feelings: ");
          
@@ -91,8 +95,50 @@ class Sillystory {
          String place1 = myScanner.nextLine();
          String place2 = myScanner.nextLine();
 
-    
-     }
+         System.out.println("And here's the story....");
+         String wait = myScanner.nextLine();
+
+        System.out.println("As " + name1 + "came riding into town on their horse, they knew something was " + adj9 + ". They could smell something he hadn't smelt in a long time.\n\n");
+        String wait = myScanner.nextLine();
+        System.out.println("It was "+ noun3 +". This could only mean one thing, Captain "+ noun3 + "was in town.");
+        String wait = myScanner.nextLine();
+        System.out.println(name1 +" knew they had to do something. He looked through his bag. In it, he found a picture of his true love " + name2 + ", as well as some letters sent from "+name3+".");
+        String wait = myScanner.nextLine();
+        System.out.println("But most important of all he found his" + noun1 + ", hiding behind a " + noun2 + ".");
+        String wait = myScanner.nextLine();
+        System.out.println("He picked up his " + noun1 + "which was a little " + adj1 + "and " + adj2 + "but that was OK, because he could always put it in his " + noun2 + ".");
+
+
+
+        //TODO:
+        System.out.println("So, with his <NOUN1> in his <NOUN2> he went off to find Captain <NOUN3>.");
+        String wait = myScanner.nextLine();
+        System.out.println("<NAME1> found Captain <NOUN3> very soon, becaue the smell of <NOUN3> reminded him of <NOUN4>s.");
+        String wait = myScanner.nextLine();
+        System.out.println("<NAME1> was surprised when he first saw Captain <NOUN3> because Captain <NOUN3> was actually very <ADJ3>. <NAME1> thought that Captain <NOUN3> would be more <ADJ4>.");
+        String wait = myScanner.nextLine();
+        System.out.println("But there was no time to lose, <NAME1> had to move fast, in order to save <NAME2> from Captain <NOUN3>'s boss, Master <NOUN4> the most <ADJ5> in the world.");
+        String wait = myScanner.nextLine();
+        System.out.println("<NAME1> grabbed his secret weapon, his <NOUN5> and used it to <VERB1> Captain <NOUN3> in the <NOUN6>. Captain <NOUN3> was feeling very <FEEL1>.");
+        String wait = myScanner.nextLine();
+        System.out.println("Next <NAME1> went on to see Master <NOUN4> at the <PLACE1>. It was a very <ADJ6> place, with a lot of <NOUN7>s. <NAME1> felt very <FEEL2>, but felt better when he saw ");
+        String wait = myScanner.nextLine();
+        System.out.println("picture of <NAME4>.... Wait ?! who was he looking for ?");
+        String wait = myScanner.nextLine();
+        System.out.println("He got a tap on the shoulder, it was <NAME5> coming all the way from <PLACE1> with a very <ADJ7> <NOUN8>, saying \"<NAME1>!!! You forgot your <NOUN9>, it's a really <ADJ8> <NOUN9>");
+        String wait = myScanner.nextLine();
+        System.out.println("So <NAME1> took his <ADJ8> <NOUN9> to <PLACE2> where Master <NOUN4> was waiting...");
+        String wait = myScanner.nextLine();
+        System.out.println("What happens next ?");
+        String wait = myScanner.nextLine();
+        System.out.println("To be <VERB2>....");
+        String wait = myScanner.nextLine();
+        System.out.println("I mean, to be continued....");
+        String wait = myScanner.nextLine();
+
+
+
+    }
 
 
      public static void main(String args[]){
