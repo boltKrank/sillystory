20491cb96b36cc44154b39423566047ff113890c
Stephen P. Potter <spp@unixsa.net>
2020-12-30 16:56:58 +1100

    Initial comments

--- sillstory.java
+++ sillstory.java
@@ -1,5 +1,45 @@
 class Sillystory {
 
 
-    
+    /**
+     * Story examples placed here, tags for NOUNS, VERBS, ADJECTIVES, ADVERBS, etc.
+     * 
+     * As the <NAME1> came riding into town on their horse, he knew something was strange. He could smell something he hadn't smelt in a long time.
+     * It was <NOUN3>. This could only mean one thing, Captain <NAME3> was in town.
+     * 
+     * <NAME1> knew they had to do something. He looked through his bag. In it, he found a picture of his true love <NAME2>, as well as some letters sent from <NAME3>.
+     * But most important of all he found his <NOUN1>, hiding behind a <NOUN2>.
+     * 
+     * He picked up his <NOUN1> which was a little <ADJ1> and <ADJ2>, but that was OK, because he could always put it in his <NOUN2>. 
+     * 
+     * So, with his <NOUN1> in his <NOUN2> he went off to find Captain <NOUN3>.
+     * 
+     * <NAME1> found Captain <NOUN3> very soon, becaue the smell of <NOUN3> reminded him of <NOUN4>s.
+     * 
+     * <NAME1> was surprised when he first saw Captain <NOUN3> because Captain <NOUN3> was actually very <ADJ3>. <NAME1> thought that Captain <NOUN3> would be more <ADJ4>.
+     * 
+     * But there was no time to lose, <NAME1> had to move fast, in order to save <NAME2> from Captain <NOUN3>'s boss, Master <NOUN4> the most <ADJ5> in the world.
+     * 
+     * <NAME1> grabbed his secret weapon, his <NOUN5> and used it to <VERB1> Captain <NOUN3> in the <NOUN6>. Captain <NOUN3> was feeling very <FEEL1>.
+     * 
+     * Next <NAME1> went on to see Master <NOUN4> at the <PLACE1>. It was a very <ADJ6> place, with a lot of <NOUN7>s. <NAME1> felt very <FEEL2>, but felt better when he saw 
+     * picture of <NAME4>.... Wait ?! who was he looking for ?
+     * 
+     * He got a tap on the shoulder, it was <NAME5> coming all the way from <PLACE1> with a very <ADJ7> <NOUN8>, saying "<NAME1>!!! You forgot your <NOUN9>, it's a really <ADJ8> <NOUN9>
+     * 
+     * So <NAME1> took his <ADJ8> <NOUN9> to <PLACE2> where Master <NOUN4> was waiting...
+     * 
+     * What happens next ?
+     * 
+     * To be <VERB2>....
+     * 
+     * I mean, to be continued....
+     * 
+     *
+     * 
+     * 
+     * 
+     */
+
+
 }
